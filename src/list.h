e0b65ec8bc17acd9582a63d5a91f7cfc92107cd1
燕武 <38755286+yanwu9887@users.noreply.github.com>
2018-07-10 16:33:33 +0800

    Update list.h

--- src/list.h
+++ src/list.h
@@ -2,7 +2,16 @@
 #define LIST_H
 #include "darknet.h"
 
-//双向链表的基本操作函数，list的定义位于include/darknet.h，下述函数的具体实现在src/list.c中
+/*双向链表的基本操作函数，list的定义位于include/darknet.h
+即：
+typedef struct list{
+    int size;  //双向链表的长度
+    node *front;//指向双向链表的头
+    node *back;//指向双向链表尾
+} list;
+
+下述函数的具体实现在src/list.c中
+*/
 list *make_list();
 int list_find(list *l, void *val);
 
