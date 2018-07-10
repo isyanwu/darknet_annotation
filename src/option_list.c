7a548dc386ab6c482ae33a2f62c965b9ea88121e
燕武 <38755286+yanwu9887@users.noreply.github.com>
2018-07-10 17:20:34 +0800

    Update option_list.c

--- src/option_list.c
+++ src/option_list.c
@@ -10,18 +10,18 @@ list *read_data_cfg(char *filename)
     if(file == 0) file_error(filename);
     char *line;
     int nu = 0;
-    list *options = make_list();
-    while((line=fgetl(file)) != 0){
+    list *options = make_list(); //该函数定义在src/list.c中，用于创建一个空的双向链表list，list定义在include/darknet.h中
+    while((line=fgetl(file)) != 0){//fgetl读取文件的一行，定义在src/utils.c中
         ++ nu;
-        strip(line);
+        strip(line); //删除制表、空格、换行符，定义在src/utils.c中
         switch(line[0]){
             case '\0':
             case '#':
             case ';':
-                free(line);
+                free(line);//这三个字符代表当前行无效
                 break;
             default:
-                if(!read_option(line, options)){
+                if(!read_option(line, options)){ //成功返回1，失败返回0，定义在该文件中
                     fprintf(stderr, "Config file error line %d, could parse: %s\n", nu, line);
                     free(line);
                 }
@@ -29,7 +29,7 @@ list *read_data_cfg(char *filename)
         }
     }
     fclose(file);
-    return options;
+    return options; //至此，文件读取完毕（key-value形式），每行的数据都存到list->node中
 }
 
 metadata get_metadata(char *file)
@@ -55,19 +55,19 @@ int read_option(char *s, list *options)
     size_t len = strlen(s);
     char *val = 0;
     for(i = 0; i < len; ++i){
-        if(s[i] == '='){
-            s[i] = '\0';
-            val = s+i+1;
+        if(s[i] == '='){  
+            s[i] = '\0'; //'='替换为'\0'
+            val = s+i+1; //val指向字符'\0'后面的第一个字符，相当于把一行拆开，例如 "data=cifar" 拆成了"data"和"cifar"
             break;
         }
     }
-    if(i == len-1) return 0;
+    if(i == len-1) return 0; //一行中没有字符"="
     char *key = s;
-    option_insert(options, key, val);
+    option_insert(options, key, val);//key-value结构，插入到node->val中
     return 1;
 }
 
-void option_insert(list *l, char *key, char *val)
+void option_insert(list *l, char *key, char *val) //把key插入到node->val中
 {
     kvp *p = malloc(sizeof(kvp));
     p->key = key;
@@ -88,7 +88,7 @@ void option_unused(list *l)
     }
 }
 
-char *option_find(list *l, char *key)
+char *option_find(list *l, char *key)  //在list中查找key对应的value
 {
     node *n = l->front;
     while(n){
